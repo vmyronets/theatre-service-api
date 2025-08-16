e838390c3e25ce942d197c6c71f23e443390c826
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-16 22:00:14 +0300

    Add PlayViewSet to handle Play listing, creation, retrieval, and image uploads

--- theatre/views.py
+++ theatre/views.py
@@ -1,12 +1,17 @@
 from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from theatre.models import Genre, Actor, TheatreHall
+from theatre.models import Genre, Actor, TheatreHall, Play
 from theatre.serializers import (
     GenreSerializer,
     ActorSerializer,
     TheatreHallSerializer,
+    PlaySerializer,
+    PlayListSerializer,
+    PlayDetailSerializer,
+    PlayImageSerializer,
 
 )
 
@@ -38,3 +43,66 @@ class TheatreHallViewSet(
     serializer_class = TheatreHallSerializer
 
 
+class PlayViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet
+):
+    queryset = Play.objects.prefetch_related("genre", "actors")
+    serializer_class = PlaySerializer
+
+    @staticmethod
+    def _params_to_integers(query_str):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in query_str.split(",")]
+
+    def get_queryset(self):
+
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._params_to_integers(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self._params_to_integers(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+
+        if self.action == "upload_image":
+            return PlayImageSerializer
+
+        return PlaySerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image"
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading an image to a play."""
+        play = self.get_object()
+        serializer = self.get_serializer(play, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        """Get all plays."""
+        return super().list(request, *args, **kwargs)
