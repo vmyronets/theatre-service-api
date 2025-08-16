66e6a62b93f39e30d2a125a8eb963b2d22252976
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-16 19:50:13 +0300

    Add `full_name` property to Actor model and update `__str__` method

--- theatre/models.py
+++ theatre/models.py
@@ -27,11 +27,15 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
     class Meta:
         ordering = ["last_name", "first_name"]
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.full_name
 
 
 class Play(models.Model):
