26342c0df74bae68aaa04fe224baca3cdd818867
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-16 20:33:18 +0300

    Register router and URL mapping for TheatreHallViewSet in 'theatre' app

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theatre.views import GenreViewSet, ActorViewSet
+from theatre.views import GenreViewSet, ActorViewSet, TheatreHallViewSet
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
