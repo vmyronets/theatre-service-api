ea87ffcfe080ba9f247346f9d43aceffde5bab5b
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-16 19:57:16 +0300

    Register router and URL mapping for ActorViewSet in 'theatre' app

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theatre.views import GenreViewSet
-
+from theatre.views import GenreViewSet, ActorViewSet
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
