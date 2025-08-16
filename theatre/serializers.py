47e193ed5987757c94808efd0edb284c64948bfa
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-16 20:18:59 +0300

    Add TheatreHallSerializer to handle TheatreHall data serialization

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -21,3 +21,9 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ("id", "first_name", "last_name", "full_name")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
