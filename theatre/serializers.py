3f459dfb54f67682ac4daf0c46d8f7b0df8bfce6
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-16 23:22:01 +0300

    Add PerformanceListSerializer to extend PerformanceSerializer with additional fields

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -71,3 +71,29 @@ class PerformanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Performance
         fields = ("id", "play", "theatre_hall", "show_time")
+
+
+class PerformanceListSerializer(PerformanceSerializer):
+    play_title = serializers.CharField(source="play.title", read_only=True)
+    play_image = serializers.ImageField(source="play.image", read_only=True)
+    theatre_hall_name = serializers.CharField(
+        source="theatre_hall.name",
+        read_only=True
+    )
+    theatre_hall_capacity = serializers.IntegerField(
+        source="theatre_hall.capacity",
+        read_only=True
+    )
+    tickets_available = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Performance
+        fields = (
+            "id",
+            "show_time",
+            "play_title",
+            "play_image",
+            "theatre_hall_name",
+            "theatre_hall_capacity",
+            "tickets_available"
+        )
