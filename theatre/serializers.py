e00724208b2d688042192b155a6eb08c7c7d9a6c
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-16 19:52:16 +0300

    Add ActorSerializer to handle Actor data serialization

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -15,3 +15,9 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
