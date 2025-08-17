4e6e8ced89d8876060e84f07c27520fa8f13f6df
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-17 23:33:03 +0300

    Register ReservationViewSet in URLs

--- theatre/urls.py
+++ theatre/urls.py
@@ -7,6 +7,7 @@ from theatre.views import (
     TheatreHallViewSet,
     PlayViewSet,
     PerformanceViewSet,
+    ReservationViewSet
 )
 
 router = routers.DefaultRouter()
@@ -15,6 +16,7 @@ router.register("actors", ActorViewSet)
 router.register("theatre_halls", TheatreHallViewSet)
 router.register("plays", PlayViewSet)
 router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
