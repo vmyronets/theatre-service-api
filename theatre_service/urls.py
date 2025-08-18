e7bd5e0a1fb5da52605382ff62bafa03ae51c345
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-18 19:54:42 +0300

    Add user-related endpoints to urlpatterns

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/theatre/", include("theatre.urls", namespace="theatre"))
+    path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user"))
 ]
