8748aa4159dc6fe81be307f71cd2819ae3960330
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-20 18:55:27 +0300

    Increase minimum password length in UserSerializer to 8 characters

--- user/serializers.py
+++ user/serializers.py
@@ -6,8 +6,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
-        read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        read_only_fields = ("id", "is_staff",)
+        extra_kwargs = {"password": {"write_only": True, "min_length": 8}}
 
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
