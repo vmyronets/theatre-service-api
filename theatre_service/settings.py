68eb09a020638fa2707686f16310107fbe09dd6b
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-21 22:01:03 +0300

    Add request throttling configuration to REST framework settings

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -134,6 +134,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "15/day", "user": "100/day"},
     # "DEFAULT_AUTHENTICATION_CLASSES": (
     #     "rest_framework_simplejwt.authentication.JWTAuthentication",
     # ),
