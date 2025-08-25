0a45bb326613d0d081e1858814d9aacf9c2bafdc
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-08-25 13:13:06 +0300

    Update .gitignore and adjust MEDIA_ROOT path in settings

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -129,7 +129,7 @@ USE_TZ = False
 STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/files/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
